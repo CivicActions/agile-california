2dee814117bf9f5dd99e565d2110ee0ba1d1436b
aravenrose <raven_web@749006.no-reply.drupal.org>
2016-05-23 22:39:40 -0600

    Added D8

---
+++ docroot/core/lib/Drupal/Component/Uuid/Pecl.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\Component\Uuid;
+
+/**
+ * UUID implementation using the PECL extension.
+ */
+class Pecl implements UuidInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function generate() {
+    return uuid_create(UUID_TYPE_DEFAULT);
+  }
+}
